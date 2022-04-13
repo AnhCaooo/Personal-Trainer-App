ed3ee07ae5f1670301345581b23d90c86c2432c0
Anh Cao <anhcao563@gmail.com>
2022-04-13 21:29:20 +0300

    Added icons for Customers and Training routes, added PageNotFound route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import "./App.css";
-import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Customers from "./components/routes/Customers";
 import Trainings from "./components/routes/Trainings";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import PageNotFound from "./components/routes/PageNotFound";
 
 function App() {
   return (
     <div className="App">
-      <Customers />
-      <Trainings />
+      <BrowserRouter>
+        <Link to="/">
+          <PeopleAltIcon /> Customers
+        </Link>{" "}
+        <Link to="/trainings">
+          <CalendarMonthOutlinedIcon /> Trainings
+        </Link>{" "}
+        <Routes>
+          <Route exact path="/" element={<Customers />} />
+          <Route path="trainings" element={<Trainings />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
